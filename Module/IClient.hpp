26f54914bc134aff7cd7cb8293be3bae1e796390
Arnaud WURMEL <arnaud.wurmel@epitech.eu>
2017-11-14 16:14:03 +0100

    IClient API

--- Module/IClient.hpp
+++ Module/IClient.hpp
@@ -5,6 +5,8 @@
 #ifndef ZIA_ICLIENT_HPP
 #define ZIA_ICLIENT_HPP
 
+#include "IHttpRequest.hpp"
+
 namespace zia
 {
     class IClient
@@ -12,7 +14,10 @@ namespace zia
     public:
         virtual ~IClient() {}
 
-
+        virtual IHttpRequest&   getHttpRequest() = 0;
+        virtual IHttpRequest const&   getHttpRequest() const = 0;
+        virtual IHttpRequest&   getHttpResponse() = 0;
+        virtual IHttpRequest const&   getHttpResponse() const = 0;
     };
 }
 
