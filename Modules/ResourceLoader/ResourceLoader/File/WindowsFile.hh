8de92e2da4155750152ff359dd63edb5a2490cbc
thibaud MARCHAND <thibaud.marchand@epitech.eu>
2018-02-21 15:14:54 +0100

    windonws files'

---
+++ Modules/ResourceLoader/ResourceLoader/File/WindowsFile.hh
@@ -0,0 +1,30 @@
+//
+// Created by marcha_1 on 13/02/2018.
+//
+
+#ifndef RESOURCELOADER_WINDOWSFILE_HH
+#define RESOURCELOADER_WINDOWSFILE_HH
+
+# include <sys/stat.h>
+# include "AFile.hh"
+
+#ifndef _WIN32
+namespace zia::module {
+	class WindowsFile : public AFile {
+	public:
+		WindowsFile();
+		~WindowsFile() override;
+
+	public:
+		bool    load(std::string const&) override;
+		bool    isDir() const override;
+		std::string getFullPath() const override;
+
+	private:
+		DWORD WINAPI _attributes;
+		std::string _path;
+	};
+}
+#endif
+
+#endif //RESOURCELOADER_WINDOWSFILE_HH
